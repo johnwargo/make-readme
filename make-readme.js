1a5bfccdf9f8226310af1417389d9e94a00e4fa1
John M. Wargo <john@johnwargo.com>
2015-03-25 21:07:39 -0400

    Fixed issue when folder contains spaces

--- make-readme.js
+++ make-readme.js
@@ -32,19 +32,16 @@ var slashN = '\n';
 //=================================
 //Get the current folder
 var currFolder = process.cwd();
-//Calculate the readme file name
+//Calculate the readme file name, always use the current folder
 var theFile = path.join(currFolder, fileName);
-//Now figure out the title for the readme file
-//Split the path into multiple parts (using path delimeter)
-var pathParts = currFolder.split(path.sep);
-//Then grab the last entry in the array
-var theHeading = pathParts[pathParts.length - 1];
-//Make a line of equal signs that match the lengh of the header
-var theEqualSigns = new Array(theHeading.length + 1).join("=");
-
 //Does the file already exist?
 if (!fs.existsSync(theFile)) {
-  //Create the file
+  //Figure out the header (title) for the readme file
+  //Grab the last entry in the array (the current folder name)
+  var theHeading = path.basename(currFolder).toUpperCase();
+  //Make a line of equal signs that match the lengh of the header
+  var theEqualSigns = new Array(theHeading.length + 1).join("=");
+  //Now create the file
   var wstream = fs.createWriteStream(theFile);
   //Write some stuff to the file
   //First the name of the folder we're in
@@ -60,15 +57,14 @@ if (!fs.existsSync(theFile)) {
   //close the file
   wstream.end();
   //All done
-  console.log("readme.md file created");
-
-  //Once we've created the file, launch it
+  console.log("File created");
+  //Now that we've created the file, launch it...
   if (isWindows) {
-    cmdStr = "start " + theFile;
+    cmdStr = 'start ' + fileName;
   } else {
-    cmdStr = "open " + theFile;
+    cmdStr = 'open ./' + fileName;
   }
-  console.log("Launching %s", theFile);
+  console.log("Launching readme.md");
   var child = exec(cmdStr, function (error, stdout, stderr) {
     if (error !== null) {
       console.log("\nexec error: %s\n".error, error);
