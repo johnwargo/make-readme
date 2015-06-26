f15e0d921fe20712f1fe5b1821cf68ef742cb4c8
John M. Wargo <john@johnwargo.com>
2015-06-26 17:00:27 -0400

    added jshint

--- make-readme.js
+++ make-readme.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* jshint node: true, strict: true, undef: true, unused: true */
 
 //========================================================================
 // Make-Readme
@@ -11,9 +12,9 @@
 "use strict";
 
 var exec = require('child_process').exec,
-  fs = require('fs'),
-  os = require('os'),
-  path = require('path');
+    fs = require('fs'),
+    os = require('os'),
+    path = require('path');
 
 //=================================
 // some variables
@@ -24,23 +25,23 @@ var fileName = "readme.md";
 var slashN = '\n';
 
 function launchFile() {
-  //Used to build the command string to launch the readme file
-  var cmdStr;
-  //What OS are we running on?
-  if (os.type().indexOf('Win') === 0) {
-    //Are we running on Windows? then use start
-    cmdStr = 'start ' + fileName;
-  } else {
-    //OS X or Linux, use open
-    cmdStr = 'open ./' + fileName;
-  }
-  console.log("Launching readme.md");
-  var child = exec(cmdStr, function (error, stdout, stderr) {
-    if (error !== null) {
-      console.log("\nexec error: %s\n".error, error);
-      process.exit(1);
+    //Used to build the command string to launch the readme file
+    var cmdStr;
+    //What OS are we running on?
+    if (os.type().indexOf('Win') === 0) {
+        //Are we running on Windows? then use start
+        cmdStr = 'start ' + fileName;
+    } else {
+        //OS X or Linux, use open
+        cmdStr = 'open ./' + fileName;
     }
-  });
+    console.log("Launching readme.md");
+    var child = exec(cmdStr, function (error, stdout, stderr) {
+        if (error !== null) {
+            console.log("\nexec error: %s\n".error, error);
+            process.exit(1);
+        }
+    });
 }
 
 //=================================
@@ -52,34 +53,34 @@ var currFolder = process.cwd();
 var theFile = path.join(currFolder, fileName);
 //Does the file already exist?
 if (!fs.existsSync(theFile)) {
-  //Figure out the header (title) for the readme file
-  //Grab the last entry in the array (the current folder name)
-  var theHeading = path.basename(currFolder).toUpperCase();
-  //Make a line of equal signs that match the lengh of the header
-  var theEqualSigns = new Array(theHeading.length + 1).join("=");
-  //Now create the file
-  var wstream = fs.createWriteStream(theFile);
-  //Create the finish event that fires after the write completes
-  wstream.on('finish', function () {
-    console.log("File created");
-    //Now that we've created the file, launch it... 
-    launchFile();
-  });
-  //Write some stuff to the file
-  //First the name of the folder we're in
-  wstream.write(theHeading + slashN);
-  //Next a line of equal signs (=) to set the title as a heading
-  wstream.write(theEqualSigns + slashN);
-  //A couple of blank lines
-  wstream.write(slashN + slashN);
-  //Horizontal rule
-  wstream.write("* * *" + slashN + slashN);
-  //shameless plug
-  wstream.write("ReadMe.md file created by make-readme by [John M. Wargo](http://www.johnwargo.com) - be sure to remove this line from the file");
-  //close the file
-  wstream.end();
+    //Figure out the header (title) for the readme file
+    //Grab the last entry in the array (the current folder name)
+    var theHeading = path.basename(currFolder).toUpperCase();
+    //Make a line of equal signs that match the lengh of the header
+    var theEqualSigns = new Array(theHeading.length + 1).join("=");
+    //Now create the file
+    var wstream = fs.createWriteStream(theFile);
+    //Create the finish event that fires after the write completes
+    wstream.on('finish', function () {
+        console.log("File created");
+        //Now that we've created the file, launch it...
+        launchFile();
+    });
+    //Write some stuff to the file
+    //First the name of the folder we're in
+    wstream.write(theHeading + slashN);
+    //Next a line of equal signs (=) to set the title as a heading
+    wstream.write(theEqualSigns + slashN);
+    //A couple of blank lines
+    wstream.write(slashN + slashN);
+    //Horizontal rule
+    wstream.write("* * *" + slashN + slashN);
+    //shameless plug
+    wstream.write("ReadMe.md file created by make-readme by [John M. Wargo](http://www.johnwargo.com) - be sure to remove this line from the file");
+    //close the file
+    wstream.end();
 } else {
-  console.log('The readme.md file already exists in this folder');
-  //go ahead and launch the existing file
-  launchFile();
+    console.log('The readme.md file already exists in this folder');
+    //go ahead and launch the existing file
+    launchFile();
 }
